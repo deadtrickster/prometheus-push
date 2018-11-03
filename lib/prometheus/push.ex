ab36727f483b58198b328ce2b0a6be019c5fa88f
Christopher J. McClellan <cmcclellan@pillartechnology.com>
2018-11-03 11:09:20 -0400

    Add some documentation to make credo happy

--- lib/prometheus/push.ex
+++ lib/prometheus/push.ex
@@ -1,9 +1,21 @@
 defmodule Prometheus.Push do
 
   @moduledoc """
+    Prometheus.Push works with the [prometheus.ex](https://github.com/deadtrickster/prometheus.ex/) library to push metrics to a [Pushgateway](https://github.com/prometheus/pushgateway).
+    The Pushgateway is used for short running jobs that may not give Prometheus enough time to scrape metrics.
 
+    To use this module [create a metric](https://hexdocs.pm/prometheus_ex/Prometheus.Metric.html#content), then `push` when appropriate.
   """
 
+  @doc """
+  Pushes metrics to the Pushgateway
+
+  ## Examples
+
+      iex> Prometheus.Push.push(%{job: "qwe",
+                       grouping_key: [{"abra", "kadabra"}]})
+
+  """
   def push(options) do
     :prometheus_push.push(options)
   end
