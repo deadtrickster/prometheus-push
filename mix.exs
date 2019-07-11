7eb55cc2c06009989b6fc2a6d45462889dd86319
Berni <bernhard@fittrack.io>
2019-07-11 21:13:25 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -1,29 +1,32 @@
 defmodule PrometheusPush.Mixfile do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.1.0"
 
   def project do
-    [app: :prometheus_push,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     name: "Prometheus Push",
-     deps: deps(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.html": :test],
-     docs: [main: Prometheus,
-            source_ref: "v#{@version}",
-            source_url: "https://github.com/deadtrickster/prometheus-push",
-            extras: []]]
+    [
+      app: :prometheus_push,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      name: "Prometheus Push",
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.html": :test],
+      docs: [
+        main: Prometheus,
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/deadtrickster/prometheus-push",
+        extras: []
+      ]
+    ]
   end
 
   def application do
-    [applications: [:logger,
-                    :prometheus_ex]]
+    [applications: [:logger, :prometheus_ex]]
   end
 
   defp description do
@@ -33,24 +36,28 @@ defmodule PrometheusPush.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Ilya Khaprov"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/deadtrickster/prometheus-push",
-              "Prometheus.erl" => "https://hex.pm/packages/prometheus",
-              "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex",
-              "Inets HTTPD Exporter" => "https://hex.pm/packages/prometheus_httpd",
-              "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
-              "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
-              "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
-              "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"}]
+    [
+      maintainers: ["Ilya Khaprov"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/deadtrickster/prometheus-push",
+        "Prometheus.erl" => "https://hex.pm/packages/prometheus",
+        "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex",
+        "Inets HTTPD Exporter" => "https://hex.pm/packages/prometheus_httpd",
+        "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
+        "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
+        "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
+        "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"
+      }
+    ]
   end
 
   defp deps do
-    [{:prometheus_ex, "~> 3.0"},
-
-     {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
-
-     {:ex_doc, "~> 0.15", only: :dev},
-     {:earmark, ">= 0.0.0", only: :dev}]
+    [
+      {:prometheus_ex, "~> 3.0"},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.15", only: :dev},
+      {:earmark, ">= 0.0.0", only: :dev}
+    ]
   end
 end
